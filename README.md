56bd109b4a69762275adc8c5af81c6c32655c575
emipa606 <emil.offermo@litet.net>
2021-06-12 19:20:10 +0200

    First publish

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # NoVanillaWeapons
+
+![Image](https://i.imgur.com/WAEzk68.png)
+
+
+If you have any suggestion on more properties to sort on, leave a comment here or on the Discord-server!
+
+Tries to remove all vanilla weapons from the game.
+
+To not cause to much hard errors it iterates through all weapons and sets properties to values that make them not spawn in the game and their recipes not usable for any faction. It deos not remove weapons from saves or scenario-starts but no new weapons should spawn.  
+
+For players that want to only use and find modded weapons.
+
+Base-classes are untouched so they still can be used by mods.
+
+[table]
+	[tr]
+		[td]https://invite.gg/Mlie]![Image](https://i.imgur.com/zdzzBrc.png)
+[/td]
+		[td]https://github.com/emipa606/NoVanillaWeapons]![Image](https://i.imgur.com/kTkpTOE.png)
+[/td]
+	[/tr]
+[/table]
+
+![Image](https://i.imgur.com/Rs6T6cr.png)
+
+
+
+-  See if the the error persists if you just have this mod and its requirements active.
+-  If not, try adding your other mods until it happens again.
+-  Post your error-log using https://steamcommunity.com/workshop/filedetails/?id=818773962]HugsLib and command Ctrl+F12
+-  For best support, please use the Discord-channel for error-reporting.
+-  Do not report errors by making a discussion-thread, I get no notification of that.
+-  If you have the solution for a problem, please post it to the GitHub repository.
+
+
+
---
+++ Source/NoVanillaWeapons/NoVanillaWeapons.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace NoVanillaWeapons
+{
+    [StaticConstructorOnStartup]
+    internal static class NoVanillaWeapons
+    {
+        static NoVanillaWeapons()
+        {
+            var vanillaWeapons = (from ThingDef weapon in DefDatabase<ThingDef>.AllDefsListForReading
+                where weapon?.IsWeapon == true &&
+                      weapon.modContentPack?.PackageId?.Contains(ModContentPack.CoreModPackageId) == true
+                select weapon).ToList();
+
+            foreach (var thingDef in vanillaWeapons)
+            {
+                thingDef.destroyOnDrop = true;
+                thingDef.generateCommonality = 0;
+                thingDef.thingSetMakerTags?.Clear();
+                thingDef.weaponTags?.Clear();
+                thingDef.generateAllowChance = 0;
+                thingDef.recipeMaker = null;
+                thingDef.scatterableOnMapGen = false;
+                thingDef.tradeability = Tradeability.None;
+                thingDef.tradeTags?.Clear();
+            }
+
+            for (var i = vanillaWeapons.Count - 1; i > 0; i--)
+            {
+                GenGeneric.InvokeStaticMethodOnGenericType(typeof(DefDatabase<>), typeof(ThingDef), "Remove",
+                    vanillaWeapons[i]);
+            }
+
+            var weaponRecipes = from recipe in DefDatabase<RecipeDef>.AllDefsListForReading
+                where vanillaWeapons.Contains(recipe.ProducedThingDef) || (from product in recipe.products
+                    where vanillaWeapons.Contains(product.thingDef)
+                    select product).Any()
+                select recipe;
+
+            foreach (var weaponRecipe in weaponRecipes)
+            {
+                weaponRecipe.factionPrerequisiteTags = new List<string> {"NotForYou"};
+            }
+        }
+    }
+}
